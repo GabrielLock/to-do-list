d48e75964c91f467cb983d8da05876b03c77da90
GabrielLock <gabrielferreiralockvieira@gmail.com>
2023-05-07 11:27:42 -0300

    middlewares

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+//olha na requisição se tem o json e coloca no req.body
+
+const log = (req, res, next) =>{
+    console.log(req.body);
+    console.log(`Data: ${Date.now()}`);
+    next();
+    //tem como usar middlewares de bibliotecas ou criar os proprios middlewares
+}
+
+app.use(log);
+
+app.get('/', (req, res) => {
+    res.send('<h1>Minha Lista de Tarefas :)</h1>');
+})
+
+app.get('/json', (req, res) => {
+    res.json({title: "Tarefa X", done: true});
+    //respondendo em json
+})
+
+app.listen(3000, () => {
+    console.log('Servidor Ativo!');
+})
+
+//instalando nodemon - para que não precise ficar restartando o servidor manualmente, ele fara automaticamente.
+//npm install nodemon --save
